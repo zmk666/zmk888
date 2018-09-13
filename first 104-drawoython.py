cd12f5d43451e7c06f55fbbf7e5a8c9b527c23af
zmk666 <43244659+zmk666@users.noreply.github.com>
2018-09-13 23:15:42 +0800

    Add files via upload

---
+++ first 104-drawoython.py
@@ -0,0 +1,33 @@
+#Drawpython.py
+import turtle
+turtle.setup(650,350,200,200)
+turtle.penup()
+turtle.fd(-250)
+turtle.pendown()
+turtle.pensize(25)
+turtle.pencolor("black")
+turtle.seth(-40)
+for i in range(2):
+    turtle.pencolor("purple")
+    turtle.circle(40,80)
+    turtle.pencolor("red")
+    turtle.circle(-40,80)
+for i in range(2):
+    turtle.pencolor("darkgreen")
+    turtle.circle(40,80)
+    turtle.pencolor("gold")
+    turtle.circle(-40,80/2)
+turtle.circle(40,40)
+tuitle.fd(40)
+turtle.circle(16,180)
+turtle.fd(26)
+turtle.penup()
+turtle.fd(20)
+tuetle.pensize(5)
+turtle.pendown()
+turtle.fd(20)
+
+
+
+
+    
