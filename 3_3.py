a0f075937edd53ac11aa72c1ccde2825d651a076
zmk666 <43244659+zmk666@users.noreply.github.com>
2018-10-19 10:34:56 +0800

    Update 3_3.py

--- 3_3.py
+++ 3_3.py
@@ -1,9 +1,7 @@
-
-      
-        
 dayup, dayfactor = 1.0, 0.01
-for i in range(1,366):
-    if i % 10 in [4,5,6,7]:
+for i in range(365):
+    if i % 11 in [3,4,5,6]:
         dayup = dayup * (1 + dayfactor)
-    print("{}：天的能力{}".format(i+1,dayup))
+    else:
+      dayup=dayup
 print("连续学习3天能力值不变，从第4天至第7天每天能力增长为前一天1%，每10天休息一天的力量: {:.2f}.".format(dayup))
